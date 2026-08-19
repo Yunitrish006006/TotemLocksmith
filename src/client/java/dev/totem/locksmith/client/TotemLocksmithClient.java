db8a4041eddcf3dea5637d60f294cf81ff8afdc6
阿昀 <yunthomas006@gmail.com>
2026-08-19 14:32:30 +0800

    feat: enable locksmith manual recipe rendering

--- src/client/java/dev/totem/locksmith/client/TotemLocksmithClient.java
+++ src/client/java/dev/totem/locksmith/client/TotemLocksmithClient.java
@@ -1,11 +1,14 @@
 package dev.totem.locksmith.client;
 
+import dev.totem.locksmith.client.manual.LocksmithManualPageOverlay;
+import dev.totem.locksmith.client.manual.LocksmithManualRecipeCache;
 import net.fabricmc.api.ClientModInitializer;
 
 /** Client-only visual bootstrap. Server policy never depends on this class. */
 public final class TotemLocksmithClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // Attached lock rendering and the management screen consume only server snapshots.
+        LocksmithManualRecipeCache.register();
+        LocksmithManualPageOverlay.register();
     }
 }
